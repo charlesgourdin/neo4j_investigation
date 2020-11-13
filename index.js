f48f2bd0632375c618b02c2c1fb95d4c89b80ef8
Charles Gourdin <gourdin.charles@gmail.com>
2020-11-13 11:28:18 +0100

    [build] plug neo4j graph

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var neo4j = require('neo4j-driver');
+var driver = neo4j.driver('bolt://34.224.17.116:51853', neo4j.auth.basic('neo4j', 'panels-computations-concurrence'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.get('/test', (req, res) => {
+  const query = 'MATCH (p:Person) RETURN p.name LIMIT 1';
+
+  const session = driver.session();
+
+  session.run(query)
+    .then(function(result) {
+      result.records.forEach(function(record) {
+          res.send(record.get('p.name'));
+      })
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 })
 
 app.listen(port, (err) => {
   if (err) {
-      throw new Error("serveur don't listen")
+      throw new Error("serveur don't listen");
   }
-  console.log(`Server is listening on port ${port}`)
-})
+  console.log(`Server is listening on port ${port}`);
+});
